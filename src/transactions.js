028465abf88c1cf1a6a30f656959fab03c0272c9
Linet <teweilinet@gmail.com>
2024-04-27 09:00:15 +0300

    handleChange values

--- src/transactions.js
+++ src/transactions.js
@@ -3,10 +3,10 @@ import React from "react";
 function Transaction() {
     let items = [
         { date: "2019-08-07", description: "Movies", category: "Entertainment", amount: -89 },
-        { date: "2019-08-07", description: "Chipotle", category: "Food", amount: 200 },
-        { date: "2019-08-07", description: "Paycheck from Bob's Burgers", category: "Income", amount: 1000 }
+        { date: "2019-07-07", description: "Chipotle", category: "Food", amount: 200 },
+        { date: "2017-08-07", description: "Paycheck from Bob's Burgers", category: "Income", amount: 1000 }
     ];
-//the variable below iterates over the items above
+  //the variable below iterates over the items above
     const renderTransactions = items.map((item, index) => (
   //the unique key applies to every item to uniquely identify them
         <tr key={index}> 
