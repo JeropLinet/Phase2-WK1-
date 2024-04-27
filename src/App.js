3f9d86924f9b5190a5ee7896728d7c846f62c901
Linet <teweilinet@gmail.com>
2024-04-27 07:31:29 +0300

    creation of addtransaction and the table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
     return(
-        <div>
-            <h1>The Royal Bank of Flatiron</h1>
+        <div className='App'>
+             <h1 className='header'>The Royal Bank of Flatiron</h1>
+
             <div className='search'>
               <input type='text' placeholder='Search Your Transaction Here'></input>
               <button>&#128269;</button>
             </div>
+
+            <div className='addTransaction'>
+              <input type='text' placeholder='Date'></input>
+              <input type='text' placeholder='Description'></input>
+              <input type='text' placeholder='Category'></input>
+              <input type='text' placeholder='Amount'></input>
+            </div>
+            <button className='addTransaction'>Add Transaction</button>
+
+            <table className='table'>
+                <thead>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                </thead>
+
+            </table>
         </div>
     )
 }
