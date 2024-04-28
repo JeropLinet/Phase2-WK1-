2393379196e28da979a85e968b574437eb73a148
Linet <teweilinet@gmail.com>
2024-04-28 17:07:22 +0300

    type:date

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import './index.css';
 import Transaction from './transactions';
-import DatePicker from 'react-datepicker'; // Import React-Datepicker
-import 'react-datepicker/dist/react-datepicker.css';
+
 
 function App() {
     //a state is an object that represents the data that the component should keep track off
     //useState manages the state of components
     // calling usestate returns the value of the current state and allows the user to update the state
-   
+    const[date,setDate]=useState('')
     const[description,setDescription]=useState('')
     const[category,setCategory]=useState('')
-    const[amount,setAmount]=useState('')
+    const[amount,setAmount]=useState(0)
     const[transactions,setTransactions]=useState([]) //this makes sure that the transactions are always an array
     const[searchTransactions, setSearchTransactions] = useState('')
-    const [selectedDate, setSelectedDate] = useState(null)
+    
     
     //adding eventhandlers to handle change in the input
     //e.target.value access the value of the input fielld
-    
+    const handleDateChange=(e)=>setDate(e.target.value)
     const handleDescriptionChange=(e)=>setDescription(e.target.value)
     const handleCategoryChange=(e)=>setCategory(e.target.value)
     const handleAmountChange=(e)=>setAmount(e.target.value)
     const handleSearchChange = (e) => setSearchTransactions(e.target.value);
-    const handleDateChange = (date) => {
-      setSelectedDate(date);
-    };
+   
   
    //adding transactions to the table
     const addTransaction = () => {
-    const newTransaction = { selectedDate, description, category, amount } //creates a new transaction
+    const newTransaction = { date, description, category, amount } //creates a new transaction
     setTransactions([...transactions, newTransaction]) //spread operator enables creation of a new array with the existing transaction plus the new one
-    
+    setDate('')
     setDescription('')
     setCategory('')
     setAmount('')
    }
 //for the user to be able to filter through all the transactions in the table
 const allTransactions = [...transactions, // Adds transactions
-    { date: "2019-08-07", description: "Movies", category: "Entertainment", amount: -89 }, // Sample transactions
+    { date: "2019-08-07", description: "Movies", category: "Entertainment", amount:100 }, // Sample transactions
     { date: "2019-07-07", description: "Chipotle", category: "Food", amount: 200 },
+    { date: "2013-12-07", description: "Sunglasses,UrbanOutfitters", category: "Fashion", amount: 700 },
+    { date: "2000-05-07", description: "Paycheck from Bob's burgers", category: "Income", amount: 200 },
     
   ];
 //use of .filter which filters transactions based on what the user has place in the search field
@@ -66,18 +63,13 @@ const allTransactions = [...transactions, // Adds transactions
             </div>
           
             <div className='addTransaction'>
-               
-                     <DatePicker
-                     selected={selectedDate}
-                     onChange={handleDateChange}
-                     dateFormat="yyyy-MM-dd" // Date format
-                     showMonthDropdown // Show dropdown to select month
-                     showYearDropdown // Show dropdown to select year
-                     scrollableYearDropdown // Enable scrolling in year dropdown
-                     yearDropdownItemNumber={15} // Number of years displayed in the dropdown
-                     placeholderText="Select Date" // Placeholder text for the input field
-                    />
               
+              <input
+                type='date'
+                placeholder='Date' 
+                value={date}
+                onChange={handleDateChange}
+              /> 
 
               <input
                 type='text'
